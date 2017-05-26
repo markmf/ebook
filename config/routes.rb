e095d264b466edd940c9c8bfc482de3a61d8d750
Mark Francisco <markmf@yahoo.com>
2017-05-26 17:28:15 +0900

    added layers of security

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #get 'pages/home'
 
   root 'pages#home'
+  get 'dashboard' => 'pages#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
